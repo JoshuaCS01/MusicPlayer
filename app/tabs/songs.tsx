9ff549607c996d7f8ec6ca5c01c4288f5caa3b25
JoshuaCS01 <jtc55494@uga.edu>
2025-06-10 16:35:23 -0400

    Testing clicking funtionality

--- app/tabs/songs.tsx
+++ app/tabs/songs.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function SongsScreen() {
+  const navigation = useNavigation();
+
+    const goToPlaylists = () => {
+    navigation.navigate('playlists');
+  };
+
   return (
-    <View  style={styles.container} >
+    <Pressable style={styles.container} onPress={goToPlaylists}>
+    <View style={styles.container} >
       <Text style={styles.text}>Songs screen</Text>
     </View>
+    </Pressable>
   );
 }
 
